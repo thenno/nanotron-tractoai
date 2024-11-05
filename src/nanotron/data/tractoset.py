9c77e231314abe5d36bdc96477075bd238d3b890
Maksim Zuev <chiffa@tracto.ai>
2024-11-05 17:42:56 +0100

    debug print

--- src/nanotron/data/tractoset.py
+++ src/nanotron/data/tractoset.py
@@ -61,7 +61,8 @@ class TractoTableDataset(YtDataset[_T_co]):
 
     def __iter__(self) -> typing.Iterator[_T_co]:
         for batch in super().__iter__():
-            print("there is a batch ", batch)
+            import sys
+            print("there is a batch ", batch, file=sys.stderr)
             for raw in batch:
                 yield self._unfold_raw(raw)
 
