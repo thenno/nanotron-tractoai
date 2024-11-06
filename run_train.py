2ac11a4ebfcd22d4c21a2fdae2275c9b179a5088
Maksim Zuev <chiffa@tracto.ai>
2024-11-06 14:52:45 +0100

    Enable rpc driver

--- run_train.py
+++ run_train.py
@@ -286,8 +286,9 @@ def get_args():
 if __name__ == "__main__":
     toolbox = prepare_and_get_toolbox(backend=Tractorch())
     yt_client_config = yt.config.get_config(toolbox.yt_client)
-    yt_client_config["proxy"]["force_ipv4"] = True
-    yt_client_config["proxy"]["force_ipv6"] = False
+    yt_client_config["backend"] = "rpc"
+    yt_client_config["proxy"]["force_ipv4"] = False
+    yt_client_config["proxy"]["force_ipv6"] = True
     toolbox.yt_client = yt.YtClient(config=yt_client_config)
     os.environ["RANK"] = str(toolbox.coordinator.get_self_index())
     args = get_args()
