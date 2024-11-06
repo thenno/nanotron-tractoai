b6de86b4b08ac47b96b7c7bfb1eaad4e58504ed0
Maksim Zuev <chiffa@tracto.ai>
2024-11-06 16:55:50 +0100

    Disable prc

--- run_train.py
+++ run_train.py
@@ -286,13 +286,13 @@ def get_args():
 if __name__ == "__main__":
     toolbox = prepare_and_get_toolbox(backend=Tractorch())
     yt_client_config = yt.config.get_config(toolbox.yt_client)
-    yt_client_config["backend"] = "rpc"
-    yt_client_config["driver_address_resolver_config"] = {
-        "enable_ipv4": True,
-        "enable_ipv6": True,
-    }
-    yt_client_config["proxy"]["force_ipv4"] = False
-    yt_client_config["proxy"]["force_ipv6"] = True
+    # yt_client_config["backend"] = "rpc"
+    # yt_client_config["driver_address_resolver_config"] = {
+    #     "enable_ipv4": True,
+    #     "enable_ipv6": True,
+    # }
+    # yt_client_config["proxy"]["force_ipv4"] = False
+    # yt_client_config["proxy"]["force_ipv6"] = True
     toolbox.yt_client = yt.YtClient(config=yt_client_config)
     os.environ["RANK"] = str(toolbox.coordinator.get_self_index())
     args = get_args()
